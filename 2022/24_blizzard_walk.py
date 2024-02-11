174a7318b8eb2b1647fa4fd37f4cf0f13232c02e
Milan Dufek <dufek.milan@gmail.com>
2024-02-11 19:41:04 +0100

    2022: day 24 - part 2

--- 2022/24_blizzard_walk.py
+++ 2022/24_blizzard_walk.py
@@ -16,6 +16,7 @@ class Blizzard:
             '^': (-1, 0),
             'v': (1, 0),
         }
+        self.results = {1: 0, 2: 0}
 
     def parse_data(self) -> None:
         self.lines = get_data(self.input_file)
@@ -40,36 +41,48 @@ class Blizzard:
 
     def bfs(self):
         q = deque()
-        q.append((0, *self.start))
+        q.append((0, *self.start, 0))
         seen = set()
 
         while True:
-            time, r, c = q.popleft()
+            time, r, c, target = q.popleft()
 
-            if (time, r, c) in seen:
+            if (time, r, c, target) in seen:
                 continue
 
-            seen.add((time, r, c))
+            seen.add((time, r, c, target))
             time += 1
             blizzard = self.get_blizzard(time)
 
             if (r, c) not in blizzard:
-                q.append((time, r, c))
+                q.append((time, r, c, target))
 
             for dr, dc in self.DIRECTIONS.values():
                 # check if possible to reach end
-                if (r + dr, c + dc) == self.end:
-                    print(f'Part #1: {time}')
-                    exit(0)
+                if (r + dr, c + dc) == self.end and not target % 2:
+                    if target == 0:
+                        q.append((time, r + dr, c + dc, 1))
+
+                        if not self.results[1]:
+                            self.results[1] = time
+                    else:
+                        self.results[2] = time
+                        return
+
+                # go back to start
+                elif (r + dr, c + dc) == self.start and target == 1:
+                    q.append((time, *self.start, 2))
 
                 # check possible next positions
-                if 0 <= r + dr < self.nr and 0 <= c + dc < self.nc:
+                elif 0 <= r + dr < self.nr and 0 <= c + dc < self.nc:
                     if (r + dr, c + dc) not in blizzard:
-                        q.append((time, r + dr, c + dc))
+                        q.append((time, r + dr, c + dc, target))
 
     def run(self):
         self.parse_data()
         self.bfs()
+        print(f'Part #1: {self.results[1]}')
+        print(f'Part #2: {self.results[2]}')
 
 
 if __name__ == '__main__':

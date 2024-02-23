5dbafa57c9b4707037cc2552b307990453e295a9
Milan Dufek <dufek.milan@gmail.com>
2024-02-23 09:58:11 +0100

    Utils / gitignore update

--- 2023/my_utils.py
+++ 2023/my_utils.py
@@ -21,3 +21,10 @@ def get_logger(level: int = logging.INFO) -> logging:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def print_grid(grid: set) -> None:
+    max_x = max(x for x, _ in grid)
+    max_y = max(y for _, y in grid)
+    for y in range(max_y + 1):
+        print(''.join('#' if (x, y) in grid else '.' for x in range(max_x + 1)))

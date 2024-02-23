504921c4d8fbd0921aa009f7d944e4b84e2be680
Milan Dufek <dufek.milan@gmail.com>
2024-02-23 19:19:44 +0100

    2015: day 16

--- 2015/16_sues.py
+++ 2015/16_sues.py
@@ -1,7 +1,7 @@
 import re
 
 
-signature = {
+_signature = {
     'children': 3,
     'cats': 7,
     'samoyeds': 2,
@@ -17,13 +17,38 @@ signature = {
 with open('inputs/16.in') as f:
     lines = f.readlines()
 
-nums = re.compile(r'\d+')
-name = re.compile(r'(?<=\s)[a-z]+')
+re_nums = re.compile(r'\d+')
+re_name = re.compile(r'(?<=\s)[a-z]+')
 
-for line in lines:
-    vals, names = list(map(int, nums.findall(line))), name.findall(line)
-    sue_number = vals.pop(0)
 
-    if all(x == y for x, y in zip(map(signature.get, names), vals)):
-        print(list(zip(map(signature.get, names), vals)))
-        print(sue_number)
+def solve_1() -> None:
+    for line in lines:
+        vals, names = list(map(int, re_nums.findall(line))), re_name.findall(line)
+        sue_number = vals.pop(0)
+
+        if all(x == y for x, y in zip(map(_signature.get, names), vals)):
+            print(f'#1: {sue_number}')
+
+
+def indicate(name: str, x: int, y: int) -> bool:
+    if name in ('tree', 'cats'):
+        return x > y
+
+    if name in ('pomeranians', 'goldfish'):
+        return x < y
+
+    return x == y
+
+
+def solve_2() -> None:
+    for line in lines:
+        vals, names = list(map(int, re_nums.findall(line))), re_name.findall(line)
+        sue_number = vals.pop(0)
+
+        if all(indicate(name, val, _signature[name]) for name, val in zip(names, vals)):
+            print(f'#2: {sue_number}')
+
+
+if __name__ == '__main__':
+    solve_1()
+    solve_2()

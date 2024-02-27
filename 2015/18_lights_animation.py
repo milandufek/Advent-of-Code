8b3d22d0cea7041d2b40d266eebdbc14af63b786
Milan Dufek <dufek.milan@gmail.com>
2024-02-28 00:02:14 +0100

    2015: day 18

--- 2015/18_lights_animation.py
+++ 2015/18_lights_animation.py
@@ -1,20 +1,29 @@
-def solve(input_file: list, part: int):
+def sum_of_neighbors(lights: set[tuple[int, int]], x: int, y: int) -> int:
+    return sum(
+        (nx, ny) in lights
+        for nx in (x - 1, x, x + 1)
+        for ny in (y - 1, y, y + 1)
+        if (nx, ny) != (x, y)
+    )
+
+
+def solve(input_file: list, part: int) -> None:
     corners = {(0,0), (0,99), (99,0), (99,99)} if part == 2 else set()
 
     with open(input_file) as f:
-        lights = {(x,y) for y, line in enumerate(f)
-                  for x, char in enumerate(line.strip())
-                  if char == '#'}
+        lights = {
+            (x,y) for y, line in enumerate(f)
+            for x, char in enumerate(line.strip())
+            if char == '#'
+        }
         lights = corners | lights
 
-    neighbors = lambda x, y: sum((_x, _y) in lights for _x in (x - 1, x, x + 1)
-                                 for _y in (y - 1, y, y + 1)
-                                 if (_x, _y) != (x, y))
-
     for _ in range(100):
-        lights = {(x, y) for x in range(100) for y in range(100)
-                  if (x, y) in lights and 2 <= neighbors(x, y) <= 3
-                  or (x, y) not in lights and neighbors(x, y) == 3}
+        lights = {
+            (x, y) for x in range(100) for y in range(100)
+            if (x, y) in lights and 2 <= sum_of_neighbors(lights, x, y) <= 3
+            or (x, y) not in lights and sum_of_neighbors(lights, x, y) == 3
+        }
         lights = corners | lights
 
     print(len(lights))

3f979366460e53fe71fba92d33c96322420705a7
Milan Dufek <dufek.milan@gmail.com>
2024-12-27 20:37:28 +0100

    2021: day 15 - part 2

--- 2021/15_chiton.py
+++ 2021/15_chiton.py
@@ -5,8 +5,8 @@ from my_utils import get_data
 # https://adventofcode.com/2021/day/15
 
 
-def solve_1(data: list):
-    grid: list[list[int]] = [list(map(int, list(x.strip()))) for x in data]
+def solve(data: list, part: int = 1) -> int:
+    grid = get_grid(data) if part == 1 else expand_grid(data)
     rows = len(grid)
     cols = len(grid[0])
     end = (rows - 1, cols - 1)
@@ -30,6 +30,35 @@ def solve_1(data: list):
             heapq.heappush(pq, (risk + grid[nr][nc], nr, nc))
 
 
+def get_grid(data: list) -> list[list[int]]:
+    return [list(map(int, list(x.strip()))) for x in data]
+
+
+def print_grid(grid: list[list[int]]) -> None:
+    for row in grid:
+        print(''.join(map(str, row)))
+
+
+def expand_grid(data: list[list[int]]) -> list[list[int]]:
+    grid: list[list[int]] = [list(map(int, list(x.strip()))) for x in data]
+    new_grid = []
+    for i in range(5 * len(grid)):
+        new_row = []
+        for j in range(5 * len(grid[0])):
+            origin_value = grid[i % len(grid)][j % len(grid[0])]
+            increment = i // len(grid) + j // len(grid[0])
+            new_value = origin_value + increment
+            if new_value > 9:
+                new_value = new_value % 10 + 1
+            new_row.append(new_value)
+        new_grid.append(new_row)
+
+    # print_grid(new_grid)
+    return new_grid
+
+
 if __name__ == '__main__':
+    # data_input = get_data('inputs/15_example.in')
     data_input = get_data('inputs/15.in')
-    print(f'#1: {solve_1(data_input)}')
+    print(f'#1: {solve(data_input)}')
+    print(f'#2: {solve(data_input, 2)}')
